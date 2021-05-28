6a6a0edf68b7ebb30201883c9073143bb5f0163a
maedeh <maedehDoabii@gmail.com>
2021-05-28 21:40:08 +0430

    add product to project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Footer from './component/Footer/Footer'
 import Header from './component/Header/Header'
+import Home from './component/pages/Home'
 
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <Header/>
       <main className="py-3">
         <Container>
-          <h2>FC Market</h2>
+          <Home/>
         </Container>
       </main>
       <Footer/>
---
+++ src/component/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import products from '../../products'
+
+const Home = () => {
+    return (
+        <div>
+            <h1>محصولات</h1>
+            <Row>
+                {products.map((item)=>{
+                    return (
+                        <Col sm={12} md={6} lg={4}>
+                            <h3>{item.name}</h3>
+                        </Col>
+                    )
+                })}
+            </Row>
+        </div>
+    )
+}
+
+export default Home
